0876d1a8d403eca7bbe68502679f42d4de76b991
Valeriy Lazarev <v.lazarev.spb@gmail.com>
2020-12-22 18:08:12 +0300

    221220_01

---
+++ 2.JavaCore/src/com/javarush/task/task20/task2013/Solution.java
@@ -0,0 +1,72 @@
+package com.javarush.task.task20.task2013;
+
+import java.io.Externalizable;
+import java.io.IOException;
+import java.io.ObjectInput;
+import java.io.ObjectOutput;
+import java.util.List;
+
+/* 
+Externalizable Person
+*/
+
+public class Solution {
+    public static class Person implements Externalizable {
+        private String firstName;
+        private String lastName;
+        private int age;
+        private Person mother;
+        private Person father;
+        private List<Person> children;
+
+        public Person(String firstName, String lastName, int age) {
+            this.firstName = firstName;
+            this.lastName = lastName;
+            this.age = age;
+        }
+
+        public Person() {
+        }
+
+
+        public void setMother(Person mother) {
+            this.mother = mother;
+        }
+
+        public void setFather(Person father) {
+            this.father = father;
+        }
+
+        public void setChildren(List<Person> children) {
+            this.children = children;
+        }
+
+        @Override
+        public void writeExternal(ObjectOutput out) throws IOException {
+            out.writeObject(this.firstName);
+            out.writeObject(this.lastName);
+            out.writeInt(this.age);
+            out.writeObject(this.mother);
+            out.writeObject(this.father);
+            out.writeObject(this.children);
+        }
+
+        @Override
+        public void readExternal(ObjectInput in) throws IOException, ClassNotFoundException {
+            this.firstName = (String) in.readObject();
+            this.lastName = (String) in.readObject();
+            this.age = in.readInt();
+            setMother((Person) in.readObject());
+            setFather((Person) in.readObject());
+            setChildren((List) in.readObject());
+
+//            this.father = (Person) in.readObject();
+//            this.mother = (Person) in.readObject();
+//            this.children = (List<Person>) in.readObject();
+        }
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
