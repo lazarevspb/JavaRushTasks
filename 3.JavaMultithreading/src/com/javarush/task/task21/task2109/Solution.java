b9096ba6d16b873c60038911fd023f20163ba89f
Valeriy Lazarev <v.lazarev.spb@gmail.com>
2021-01-05 22:04:33 +0300

    050121_2

---
+++ 3.JavaMultithreading/src/com/javarush/task/task21/task2109/Solution.java
@@ -0,0 +1,75 @@
+package com.javarush.task.task21.task2109;
+
+/* 
+Запретить клонирование
+*/
+
+import java.util.Objects;
+
+public class Solution {
+    public static class A implements Cloneable {
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (!(o instanceof A)) return false;
+            A a = (A) o;
+            return i == a.i && j == a.j;
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(i, j);
+        }
+
+        private int i;
+        private int j;
+
+        public A(int i, int j) {
+            this.i = i;
+            this.j = j;
+        }
+
+        public int getI() {
+            return i;
+        }
+
+        public int getJ() {
+            return j;
+        }
+    }
+
+    public static class B extends A {
+        @Override
+        protected Object clone() throws CloneNotSupportedException {
+            throw new CloneNotSupportedException();
+        }
+
+        private String name;
+
+        public B(int i, int j, String name) {
+            super(i, j);
+            this.name = name;
+        }
+
+        public String getName() {
+            return name;
+        }
+    }
+
+    public static class C extends B implements Cloneable{
+        public C(int i, int j, String name) {
+            super(i, j, name);
+        }
+
+        @Override
+        protected C clone() throws CloneNotSupportedException {
+            C c = new C(this.getI(), this.getJ(), this.getName());
+            return c;
+        }
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
