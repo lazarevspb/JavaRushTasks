d9deb34a33c7e54584e07133d1a96f45897fd1c3
Valeriy Lazarev <v.lazarev.spb@gmail.com>
2021-01-11 20:36:18 +0300

    11.01.21_01

--- 3.JavaMultithreading/src/com/javarush/task/task22/task2209/Solution.java
+++ 3.JavaMultithreading/src/com/javarush/task/task22/task2209/Solution.java
@@ -1,10 +1,8 @@
 package com.javarush.task.task22.task2209;
 
 import java.io.*;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.StringTokenizer;
+import java.util.*;
+import java.util.stream.Collectors;
 
 /* 
 Составить цепочку слов
@@ -13,16 +11,12 @@ import java.util.StringTokenizer;
 public class Solution {
     public static void main(String[] args) throws IOException {
         List<String> list = new ArrayList<>();
-
-//        StringBuilder sb = new StringBuilder();
         try (BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-//             BufferedReader fileReader = new BufferedReader(new InputStreamReader(new FileInputStream(reader.readLine())))) {
-             BufferedReader fileReader = new BufferedReader(new InputStreamReader(new FileInputStream("file0")))) {
+             BufferedReader fileReader = new BufferedReader(new InputStreamReader(new FileInputStream(reader.readLine())))) {
             while (fileReader.ready()) {
                 list.add(fileReader.readLine());
             }
         }
-
 
         List<String> resultList = new ArrayList<>();
         for (String line : list) {
@@ -45,60 +39,53 @@ public class Solution {
     }
 
     public static StringBuilder getLine(String... words) {
-        StringBuilder builder = new StringBuilder();
-        if (words.length <= 1 ) return builder;
+
+        if (words.length <= 1) return new StringBuilder();
 
         List<String> list = new ArrayList<>(Arrays.asList(words));
         List<String> reultList = new ArrayList<>();
-
 
         int count = 0;
         int countSearch = 1;
         int size = list.size();
         while (!(size == reultList.size())) {
             char endCharFirstString;
-
-            if (reultList.size() == 0) {
+            if (reultList.size() == 0) {   //если первая итерация
                 endCharFirstString = Character.toLowerCase(list.get(count).charAt(list.get(count).length() - 1));
-//            } else endCharFirstString = Character.toLowerCase(builder.charAt(builder.length() - 2));
             } else {
-                String s = reultList.get(reultList.size()-1);
-                endCharFirstString = Character.toLowerCase(s.charAt(s.length()-1));
+                String s = reultList.get(reultList.size() - 1);
+                endCharFirstString = Character.toLowerCase(s.charAt(s.length() - 1));
             }
-            if(list.size() > 0){
+            if (list.size() > 0) {
                 String s = list.get(countSearch);
-            char firstCharSecondString = Character.toLowerCase(s.charAt(0));
-                if (endCharFirstString == firstCharSecondString) {
+                char firstCharSecondString = Character.toLowerCase(s.charAt(0));
+
+                if (endCharFirstString == firstCharSecondString) { // проверка букв
                     String firstWord;
                     String secondWord;
                     secondWord = list.get(countSearch);
 
-                    if (builder.length() == 0) {
+                    if (reultList.size() == 0) { // добавление на первой итерации
                         firstWord = list.get(0);
                         reultList.add(firstWord);
                         reultList.add(secondWord);
                         list.remove(firstWord);
                     } else {
-                        secondWord = list.get(countSearch);
+                        secondWord = list.get(countSearch); // добавление
                         reultList.add(secondWord);
                     }
 
 
                     list.remove(secondWord);
-//                count = 0;
                     countSearch = 0;
                 } else {
                     countSearch++;
-
                     if (countSearch == list.size()) {
-                        count++;
+                        count = 0;
                         countSearch = 0;
                         list.addAll(reultList);
                         reultList.clear();
-//                    if (count == 3)
-//                        break;
                     }
-
                 }
             } else {
                 count++;
@@ -106,17 +93,9 @@ public class Solution {
                 list.addAll(reultList);
                 reultList.clear();
             }
-
-
-
-            if (list.size() == 1) {
-//                reultList.add();
-//                list.remove(list.get(0));
-            }
-
         }
-        System.out.println("list" +  list);
-        System.out.println("reultList" + reultList);
+
+        StringBuilder builder = new StringBuilder(String.join(" ", reultList));
         return builder;
     }
 }
---
+++ 3.JavaMultithreading/src/com/javarush/task/task23/task2312/SnakeSection.java
@@ -0,0 +1,28 @@
+package com.javarush.task.task23.task2312;
+
+public class SnakeSection {
+    private int x;
+
+    public SnakeSection(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    private int y;
+
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+}
