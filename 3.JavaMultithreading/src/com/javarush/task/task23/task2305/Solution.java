1e2743e33d789aa1f694f546051b25f1d4db790c
Valeriy Lazarev <v.lazarev.spb@gmail.com>
2021-05-11 23:16:14 +0300

    11.05.21_02

---
+++ 3.JavaMultithreading/src/com/javarush/task/task23/task2305/Solution.java
@@ -0,0 +1,28 @@
+package com.javarush.task.task23.task2305;
+
+/* 
+Inner
+*/
+
+public class Solution {
+    public InnerClass[] innerClasses = new InnerClass[2];
+
+    public class InnerClass {
+    }
+
+    public static Solution[] getTwoSolutions() {
+        Solution[] result = new Solution[2];
+
+        result[0] = new Solution();
+        result[1] = new Solution();
+
+        result[0].innerClasses = new InnerClass[0];
+        result[1].innerClasses = new InnerClass[0];
+
+        return result;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
